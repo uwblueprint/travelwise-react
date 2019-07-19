3800f70af2bb43a3fc358c7ccb3806580d11b180
mattiaseyram <mattiaseyram@gmail.com>
2019-07-19 13:28:06 -0400

    Initial commit with apollo and react-router set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+import Navbar from './components/Navbar';
+import LandingPage from './components/LandingPage';
+import CompaniesPage from './components/CompaniesPage';
+
+const client = new ApolloClient({
+  uri: "https://travelwise-hasura.herokuapp.com/v1/graphql"
+});
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <Navbar />
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/companies" exact component={CompaniesPage} />
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+  return (
+      <div>
+          nav
+      </div>
+  );
+}
+
+export default Navbar;
