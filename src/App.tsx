02391e59921a3bc54d07764dd161115f51c670ca
Brandon Leung <brandonmichael.kg@gmail.com>
2019-09-26 19:32:41 -0400

    added file structure, removed serviceworkers

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 import ApolloClient from "apollo-boost";
-import Navbar from './components/Navbar';
-import LandingPage from './components/LandingPage';
-import CompaniesPage from './components/CompaniesPage';
+import Navbar from './components/Navbar/Navbar';
+import LandingPage from './components/LandingPage/LandingPage';
+import CompaniesPage from './components/CompaniesPage/CompaniesPage';
 
 const client = new ApolloClient({
   uri: "https://travelwise-hasura.herokuapp.com/v1/graphql"
