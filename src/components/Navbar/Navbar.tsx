5b8e54eece434c5659c7a13147d501b3bd1ea7b7
bruce-he-62 <bruce.he.62@gmail.com>
2019-10-03 20:12:47 -0400

    Add basic navbar without functioning tabs.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,86 @@
 import React from 'react';
+import {AppBar, Box, makeStyles, Theme, Tab, Tabs, Typography} from '@material-ui/core'
+import { tsConstructorType } from '@babel/types';
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+function TabPanel (props: TabPanelProps) {
+  const {children, value, index, ...other} = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
+
+function a11yProps(index: any) {
+  return {
+    id: `simple-tab=${index}`,
+    'aria-controls': `simple-tabpanel-${index}`
+  };
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+/*
 const Navbar: React.FC = () => {
   return (
-      <div>
-          nav
-      </div>
+    <div>
+      <AppBar position="static">
+      </AppBar>
+    </div>
   );
+}
+*/
+
+function Navbar() {
+  
+
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+ 
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="tabs">
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        Item one
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item one
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item one
+      </TabPanel>
+    </div>
+  )
+
 }
 
 export default Navbar;
