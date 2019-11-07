0a6aced3ec8b4350c945578f0e53363ea3d6658c
bruce-he-62 <bruce.he.62@gmail.com>
2019-11-06 19:53:16 -0500

    Clean up styles. Add Travelwise logo.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,96 +1,50 @@
 import React from 'react';
-import {AppBar, Box, makeStyles, Theme, Tab, Tabs, Typography} from '@material-ui/core';
+import {AppBar, Box, makeStyles, Theme, Tab, Tabs} from '@material-ui/core';
 import {Link, RouteComponentProps} from 'react-router-dom';
 import { withStyles, createStyles } from '@material-ui/styles';
+
+import "./Navbar.css"
+
+import logo from "./TravelWise.jpg"
 
 // color values
 // #71A850
 // #FFFFFF
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: any;
-  value: any;
-}
-
-function TabPanel (props: TabPanelProps) {
-  const {children, value, index, ...other} = props;
-
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      <Box p={3}>{children}</Box>
-    </Typography>
-  );
-}
-
-function a11yProps(index: any) {
-  return {
-    id: `simple-tab=${index}`,
-    'aria-controls': `simple-tabpanel-${index}`
-  };
-}
-
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     flexGrow: 1,
   },
-}));
-
-const NavbarTabs = withStyles({
-  root: {
+  TabsRoot: {
     backgroundColor: '#FFFFFF',
   },
-  indicator: {
-    
+  TabsIndicator: { 
     backgroundColor: '#71A850',
   },
-})(Tabs);
-
-const NavbarTab = withStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      textTransform: 'none',
-      minWidth: 88,
-      //fontWeight: theme.typography.fontWeightRegular,
-      //marginRight: theme.spacing(4),
-      fontFamily: 'Nunito',
-      //fontWeight: 'fontWeightBold',
-      color: "#3C3C3C",
-      '&:hover': {
-        color: '#71A850',
-        opacity: 1,
-      },
-      '&$selected': {
-        color: '#71A850',
-        //fontWeight: theme.typography.fontWeightMedium,
-      },
-      '&:focus': {
-        color: '#71A850',
-      },
+  TabRoot: {
+    textTransform: 'none',
+    minWidth: 88,
+    fontFamily: 'Nunito',
+    color: "#3C3C3C",
+    '&:hover': {
+      color: '#71A850',
+      opacity: 1,
     },
-    selected: {},
-  })
-)((props: StyledTabProps) => <Tab disableRipple {...props}/>)
+    '&$selected': {
+      color: '#71A850',
+    },
+    '&:focus': {
+      color: '#71A850',
+    },
+  },
+  TabsSelected: {},
 
-interface StyledTabProps {
-  label: string;
-}
-
-function RedirectToPage() {
-
-}
+}));
 
 interface ChildComponentProps extends RouteComponentProps<any> {
   /* other props for ChildComponent */
   //history: History
 }
+
 
 function Navbar(/*props: React.SFC<ChildComponentProps>*/) {
   const classes = useStyles();
@@ -102,24 +56,18 @@ function Navbar(/*props: React.SFC<ChildComponentProps>*/) {
 
   return (
     <div className={classes.root}>
+      {/*<img src={logo} /*style={{width: 300, height: 20}}/>*/}
+
       <AppBar position="static">
+        
 
-        <NavbarTabs value={value} onChange={handleChange} aria-label="tabs">
-          <Link to="/"><NavbarTab label="Landing Page" /*onClick={() => history.push("/")}*/ {...a11yProps(0)}/></Link>
-          <Link to="/companies"><NavbarTab label="Companies Page" /*onClick={() => history.push("/companies")}*/ {...a11yProps(1)}/></Link>
+        <Tabs classes={{root: classes.TabsRoot, indicator: classes.TabsIndicator}} value={value} onChange={handleChange} aria-label="tabs">
           
-        </NavbarTabs>
+          {/*<Link to="/">*/}<Tab classes={{root: classes.TabRoot}} label="Landing Page" /*onClick={() => history.push("/")}*//>{/*</Link>*/}
+  {/*<Link to="/companies">*/}<Tab classes={{root: classes.TabRoot}} label="Companies Page" /*onClick={() => history.push("/companies")}*//>{/* </Link>*/}
+          
+        </Tabs>
       </AppBar>
-      <TabPanel value={value} index={0}>
-        Item one
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Two
-      </TabPanel>
-
-
-    <Link to="/">landing</Link>
-    <Link to="/companies">companies</Link>
     </div>
   )
 
