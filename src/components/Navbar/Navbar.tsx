6db8386d3d1e93d419ec1d7a8819c919b5f0aaba
bruce-he-62 <bruce.he.62@gmail.com>
2019-11-07 20:11:13 -0500

    Add avatar. Extract divs & image from tabs block.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AppBar, Box, makeStyles, Theme, Tab, Tabs} from '@material-ui/core';
+import {AppBar, Avatar, Box, Grid, makeStyles, Theme, Tab, Tabs, Typography} from '@material-ui/core';
 import {Link, RouteComponentProps} from 'react-router-dom';
 import { withStyles, createStyles } from '@material-ui/styles';
 
@@ -13,10 +13,17 @@ import logo from "./TravelwiseLogo.svg";
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     flexGrow: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  AppBarRoot: {
+    display: 'flex',
+    flexDirection: 'row',
+    backgroundColor: '#FFFFFF'
   },
   TabsRoot: {
     display: 'flex',
     backgroundColor: '#FFFFFF',
+    flexGrow: 2
   },
   TabsIndicator: { 
     backgroundColor: '#71A850',
@@ -38,6 +45,13 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
   },
   TabsSelected: {},
+  UserGrid: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  AvatarStyle: {
+
+  }
 }));
 
 function Navbar() {
@@ -51,14 +65,18 @@ function Navbar() {
   return (
     <div className={classes.root}>
 
-      <AppBar position="static">
+      <AppBar classes={{root: classes.AppBarRoot}} position="static">
+        <img src={logo} style={{width: 150, height: 50}} />
+        <div style={{flexGrow: 2}}/>
         <Tabs classes={{root: classes.TabsRoot, indicator: classes.TabsIndicator}} value={value} onChange={handleChange} aria-label="tabs">
-          <img src={logo} style={{width: 150, height: 50, paddingTop: '0.333%', paddingBottom: '0.333%', paddingLeft: '4.44%'}} />
-          <div style={{flexGrow: 2}}/>
           <Tab classes={{root: classes.TabRoot}} component={Link} to="/" label="Landing Page" />
           <Tab classes={{root: classes.TabRoot}} component={Link} to="/companies" label="Companies Page"/>
-          <div style={{flexGrow: 2}}/>
         </Tabs>
+        <div style={{flexGrow: 2}}/>
+        <Grid classes={{root: classes.UserGrid}}>
+            <Typography>aslkjfd;asd</Typography>
+            <Avatar>TW</Avatar>
+          </Grid>
       </AppBar>
     </div>
   )
