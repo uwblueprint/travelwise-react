dced96fdeb27d9bc2fe2b2a6c3be91d45e36d02a
pmielnik <patricia.mielnik1@gmail.com>
2019-11-21 00:36:16 -0500

    minor updates to radio buttons

--- src/components/surveys/Radio.tsx
+++ src/components/surveys/Radio.tsx
@@ -24,17 +24,21 @@ class RadioGrid extends React.Component<QuestionProps>{
 
         return(
             <div key={id + "radio_grid"} className='radioGrid'>
+                <div className='radio-grid-header'>
                 {options.map((option, index) => (
-                    <div key={option.id} className='radio-grid-header'>
+                    <div key={option.id} className='radio-grid-header-item'>
                         {option.value}
                     </div>
                 ))}
+                </div>
+                <div className='radio-grid-row'>
                 <h3 className='radio-grid-item'>{name}</h3>
                 {options.map((option, index) => (
                     <div className='radio-grid-item'>
                         <input type ='radio' name={name} value={option.value} onChange={this.props.onChange}/>
                     </div>
                 ))}
+                </div>
             </div>
         )
     }
