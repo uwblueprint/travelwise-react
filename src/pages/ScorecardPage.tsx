fef60f7bf49ddf4e69e02655685e6fae1c72dd33
bruce-he-62 <bruce.he.62@gmail.com>
2019-11-28 21:00:01 -0500

    Temporary scorecard frontend setup

--- src/pages/ScorecardPage.tsx
+++ src/pages/ScorecardPage.tsx
@@ -3,6 +3,7 @@ import { Query } from "react-apollo";
 import { gql } from "apollo-boost";
 import DonutChart from "react-d3-donut";
 import { Card, LinearProgress, makeStyles } from "@material-ui/core";
+import { Checkbox } from "material-ui";
 
 const COMPANIES_QUERY = gql`
   {
@@ -85,6 +86,51 @@ const DonutComponent: React.FC<DonutComponentProps> = (props) => {
   )
 }
 
+interface DonutCardProps {
+  data: Array<{count: number, color: string}>;
+  innerRadius: number;
+  outerRadius: number;
+  title: string;
+  fontSize: number;
+  pieClass: string;
+  description: string;
+  //cardTitle: string;
+}
+
+const DonutCard: React.FC<DonutCardProps> = (props) => {
+  const classes = makeStyles(() => ({
+    cardStyle: {
+      border: "1.36px solid #CCCCCC",
+      maxWidth: 210,
+      minWidth: 150,
+      maxHeight: 280,
+      minHeight: 200,
+      textAlign: "center",
+      padding: 31.6,
+      width: '15vw',
+      height: '20vw',
+      display: 'flex',
+      flexDirection: 'column',
+      margin: 30
+    },
+  }))();
+
+  return(
+    <Card className={classes.cardStyle}>
+      <DonutComponent
+        data={props.data}
+        pieClass={props.pieClass} 
+        innerRadius={props.innerRadius}
+        outerRadius={props.outerRadius} 
+        fontSize={props.fontSize}
+        title={props.title} 
+      />
+      <div style={{flexGrow: 3}}/>
+      <p>{props.description}</p>
+    </Card>
+  )
+}
+
 interface LinearProgressCardProps {
   title: string,
   numerator: number,
@@ -106,6 +152,7 @@ const LinearProgressCard: React.FC<LinearProgressCardProps> = (props) => {
       height: '20vw',
       display: 'flex',
       flexDirection: 'column',
+      margin: 30
     },
     barRoot: {
       height: 5,
@@ -137,23 +184,50 @@ const LinearProgressCard: React.FC<LinearProgressCardProps> = (props) => {
   )
 }
 
+interface checkListProps {
+  data: Array<{name: string, isChecked: boolean}>;
+}
+
+const CheckListCard: React.FC<checkListProps> = (props) => {
+  return (
+    <Card>
+      {/*props.data.map(elt => 
+        <div>
+          {elt.isChecked == true ? <Checkbox disabled checked/> : <Checkbox disabled />}          
+          {elt.name}
+        </div>
+      )*/}
+    </Card>
+  )
+}
+
 
 const ScorecardPage: React.FC = () => {
   var dt = [{count: 3, color: "#1978BE"}, {count: 1, color: '#F3F3F3'}]
+  var dt2 = [{count: 15, color: "#F6C000"}, {count: 5, color: '#F3F3F3'}]
+  var flurble = [{name: "item 1", isChecked: false}, {name: "item 2", isChecked: true}]
   return (
     <div style={{margin: '20px'}}>
+      <div style={{display: 'flex', alignItems: 'row', justifyContent: 'center'}}>
+        <DonutComponent data={dt} pieClass='pie1' innerRadius={130} outerRadius={150} fontSize={30} title="Overall Score"/>
+       </div>
       
-      <DonutComponent data={dt} pieClass='pie1' innerRadius={120} outerRadius={130} fontSize={30} title="item 1"/>
-      <DonutComponent data={dt} pieClass='pie8' innerRadius={90} outerRadius={100} fontSize={15} title="item 2"/>
 
+        <div style={{display: 'flex', alignItems: 'column', justifyContent: 'center'}}>
+          <LinearProgressCard title="Target and Achievement" numerator={15} denominator={30} barColor="#1978BE"/>
+          <LinearProgressCard title="Support Structure" numerator={15} denominator={30} barColor="#71A850"/>
+          {/*<LinearProgressCard title="Mode-Specific" numerator={15} denominator={30} barColor="#F6C000"/>*/}
+          <DonutCard data={dt2} innerRadius={90} outerRadius={100} title={"Mode-Specific"} fontSize={20} pieClass={'pie2'} description={"A brief description."}/>
+        </div>
+     
 
-      <LinearProgressCard title="title" numerator={30} denominator={40} barColor="#71A850"/>
-      <LinearProgressCard title="title" numerator={30} denominator={40} barColor="#71A850"/>
-
-      <div style={{margin: '20px'}}>
+      <CheckListCard data={flurble} />
+{/*
+  <div style={{margin: '20px'}}>
         companies
         {companiesList}
       </div>
+*/}
     </div>
   );
 };
